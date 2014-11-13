4fbd07505f63df0a05bf61dd1d0ed09b8d1d5a1c
Rob Polak <rpolak@godaddy.com>
2014-11-13 07:17:02 -0700

    incremental commit

--- src/scrabbleApiController.js
+++ src/scrabbleApiController.js
@@ -93,6 +93,74 @@ module.exports = function() {
 
         return urls;
     }
+var scrabbleValues = {
+  A : 1,
+  B : 3,
+  C : 3,
+  D : 2,
+  E : 1,
+  F : 4,
+  G : 2,
+  H : 4,
+  I : 1,
+  J : 8,
+  K : 5,
+  L : 1,
+  M : 3,
+  N : 1,
+  O : 1,
+  P : 3,
+  Q : 10,
+  R : 1,
+  S : 1,
+  T : 1,
+  U : 1,
+  V : 4,
+  W : 4,
+  X : 8,
+  Y : 4,
+  Z : 10
+  };
+  var wordsWithFriendsValues = {
+    A : 1,
+    B : 4,
+    C : 4,
+    D : 2,
+    E : 1,
+    F : 4,
+    G : 3,
+    H : 3,
+    I : 1,
+    J : 10,
+    K : 5,
+    L : 2,
+    M : 4,
+    N : 2,
+    O : 1,
+    P : 4,
+    Q : 10,
+    R : 1,
+    S : 1,
+    T : 1,
+    U : 2,
+    V : 5,
+    W : 4,
+    X : 8,
+    Y : 3,
+    Z : 10
+  };
+  
+    function getScores(word) {
+      var toRet = {
+        scrabbleScore: 0,
+        wwfScore: 0
+      };
+      _.each(word, function(letter) {
+        toRet.scrabbleScore = toRet.scrabbleScore + scrabbleValues[letter.toUpperCase()];
+        toRet.wwfScore = toRet.wwfScore + wordsWithFriendsValues[letter.toUpperCase()];
+      });
+      return toRet;
+    }
 
     function groupByLength(arr) {
         var toRet = {};
@@ -107,11 +175,16 @@ module.exports = function() {
                 toRet[len] = obj;
                 arrItem = toRet[len]
             }
-            arrItem.words.push(item);
+            var score = getScores(item);
+            var word = {
+              word: item,
+              score: score
+            }
+            arrItem.words.push(word);
         });
 
         toRet = _.sortBy(toRet, function (item) {
-            return item.length;
+            return -item.length;
         });
         return toRet;
     }
